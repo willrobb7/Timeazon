1d4b6c5e688a9a6067eb3824833eb7dc380cf0fd
“adamshaw90” <adamoshaw90@gmail.com>
2026-02-26 10:38:14 +0000

    Remove commented out code

--- Marketplace/src/components/ProductCard.jsx
+++ Marketplace/src/components/ProductCard.jsx
@@ -8,9 +8,6 @@ export default function ProductCard({ product, onClick }) {
         <div className="image-wrapper" onClick={onClick}>
           <img src={product.image_url} alt={product.name} />
 
-          <button className="addToCart">+</button>
-          <p className="addPrompt">Add To Cart</p>
-
           <div className="overlay" />
 
           <div className="card-info">
@@ -24,70 +21,3 @@ export default function ProductCard({ product, onClick }) {
   );
 }
 
-
-// import { useEffect, useState } from "react"
-
-// function toTitle(product) {
-//   return product
-//     .split("_")
-//     .map(w => w.charAt(0).toUpperCase() + w.slice(1))
-//     .join(" ")
-// }
-
-// export default function ProductCards() {
-//   const baseUrl = import.meta.env.VITE_PRODUCT_CARDS_DOMAIN
-//   const [products, setProducts] = useState([])
-//   const [status, setStatus] = useState("loading")
-
-//   useEffect(() => {
-//     async function loadProducts() {
-//       try {
-//         const response = await fetch(`/api/products`)
-
-//         if (!response.ok) {
-//           throw new Error("API error")
-//         }
-
-//         const data = await response.json()
-//         setProducts(data.products)
-//         setStatus("ready")
-//       } catch {
-//         setStatus("error")
-//       }
-//     }
-
-//     loadProducts()
-//   }, [])
-
-//   if (status === "loading") {
-//     return <div>Loading products…</div>
-//   }
-
-//   if (status === "error") {
-//     return <div>Failed to load products</div>
-//   }
-
-//   return (
-    
-//         <div>{products.map(product => {
-//           const filename = `${product}.pdf`
-
-//           return (
-//             <div key={product}>
-//               <div>{toTitle(product)}</div>
-
-//               <button
-                
-//                 href={`${baseUrl}/${filename}`}
-//                 target="_blank"
-//                 rel="noreferrer"
-//               >
-                
-//               </button>
-//             </div>
-            
-//           )
-//         })}
-//       </div>
-//   )
-// }
--- Marketplace/src/components/homepagecard.jsx
+++ Marketplace/src/components/homepagecard.jsx
@@ -8,4 +8,3 @@ export default function HomePageCard(props) {
   );
 }
 
-// For the team
